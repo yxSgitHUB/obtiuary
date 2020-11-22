c805130062679e8f7154905235addd47a83a61d9
yu <yu>
2020-11-22 12:43:06 +0800

    Antd 引入以及按需加载

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React ,{useState,FC} from "react";
+import {Button} from "antd";
 import './App.css';
-function App(){
-  return <div>11123</div>
+const App:FC=()=>{
+  let [user,setUser] = useState({
+    name:"",
+    password:""
+  })
+  return <div>
+    <p>user.name:{user.name}</p>
+    <p>user.password:{user.password}</p>
+    <Button onClick={()=>{
+      setUser({
+        name:"name",
+        password: "password"
+      })
+    }}>user set name:"name"，password:"password"</Button>
+  </div>
 }
 export default App;
